40d6e14d1f6001ab4e54201039fad55ba115075b
Sebastián González <tagae@ehub.io>
2020-09-20 23:36:40 +0200

    fix builder PORTAGE_PROFILE

--- provision.d/facets/builder.sh
+++ provision.d/facets/builder.sh
@@ -3,6 +3,13 @@ test -v PROVISION_BUILDER && return || readonly PROVISION_BUILDER="$(realpath "$
 source "$LIB_DIR"/gentoo.sh
 source "$LIB_DIR"/builder.sh
 
+case $(uname -m) in
+    x86_64)
+        : ${PORTAGE_PROFILE:=default/linux/amd64/17.1/systemd} ;;
+    aarch64)
+        : ${PORTAGE_PROFILE:=default/linux/arm64/17.0/systemd} ;;
+esac
+
 sync-portage-tree
 set-portage-profile
 install-builder-packages
