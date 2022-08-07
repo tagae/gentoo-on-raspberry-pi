d1f37f88558955ba37d67ee8543f5cbae6526e29
Sebastián González <tagae@ehub.io>
2022-08-07 11:55:58 +0200

    chore(github): cache builder image

--- lib/provision.sh
+++ lib/provision.sh
@@ -4,5 +4,3 @@ test -v PROVISION_LIB && return || readonly PROVISION_LIB="$(realpath "$BASH_SOU
 
 source "$LIB_DIR"/runtime.sh
 source "$LIB_DIR"/gentoo.sh
-source "$LIB_DIR"/kernel.sh
-source "$LIB_DIR"/btrfs.sh
